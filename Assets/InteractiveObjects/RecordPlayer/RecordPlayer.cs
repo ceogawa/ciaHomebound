c47df0981920ad9b8fc5ebf637de332f2dae7106
Claire Ogawa <ceogawa@calpoly.edu>
2025-06-04 00:41:07 -0700

    records lock into place and rotate

--- Assets/InteractiveObjects/RecordPlayer/RecordPlayer.cs
+++ Assets/InteractiveObjects/RecordPlayer/RecordPlayer.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 public class RecordPlayer : MonoBehaviour
 {
     public AudioClip[] songs;
+    public AudioClip scratch;
     public AudioSource audioSource;
     private int songIndex = 0;
+    public float rotationSpeed = 100f;
+
+    private GameObject curRecord;
 
     private bool playable;
     void Start()
@@ -24,10 +28,9 @@ public class RecordPlayer : MonoBehaviour
     }
     void Update()
     {
-        // if (!audioSource.isPlaying)
-        // {
-        //     ChangeSong();
-        // }
+        if (playable == false && curRecord != null) {
+            curRecord.transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        }
     }
 
     void OnCollisionEnter(Collision collision)
@@ -41,13 +44,20 @@ public class RecordPlayer : MonoBehaviour
 
         if (otherObject.tag == "Record")
         {
-            if (otherObject.name == "record_johnny_cash" && playable == true){ audioSource.clip = songs[0]; }
-            else if (otherObject.name == "record_elvis_dont_be_cruel" && playable == true){ audioSource.clip = songs[1]; }
+            audioSource.clip = scratch;
+            audioSource.Play();
+
+            if (otherObject.name == "record_johnny_cash" && playable == true) { audioSource.clip = songs[0]; }
+            else if (otherObject.name == "record_elvis_dont_be_cruel" && playable == true) { audioSource.clip = songs[1]; }
             else if (otherObject.name == "record_carl_perkins" && playable == true) { audioSource.clip = songs[2]; }
             else if (otherObject.name == "record_isley_brothers" && playable == true) { audioSource.clip = songs[3]; }
-            else{ return; }
+            else { return; }
 
             audioSource.Play();
+            // assign current record
+            curRecord = otherObject;
+            Vector3 worldPosition = new Vector3(2.1f, 0.69f, 0.31f);
+            curRecord.transform.position = worldPosition;
             playable = false;
         }
 
